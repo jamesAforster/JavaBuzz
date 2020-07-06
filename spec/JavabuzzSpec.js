61a3c6cf94279b28915bb6c8f28735ce486ac714
James Forster <jamesalexforster@gmail.com>
2020-07-06 17:09:26 +0100

    tests written

--- spec/JavabuzzSpec.js
+++ spec/JavabuzzSpec.js
@@ -1,14 +1,22 @@
 describe('Javabuzz', function() {
 
-  var javabuzz;
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
 
   describe('knows when a number is', function() {
   
     it('divisible by 3', function() {
-      javabuzz = new Javabuzz();
-      expect(isDivisibleByThree(3)).toBe(true);
+      expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
 
+  });
+
+  describe('knows when a number is NOT', function () {
+
+    it('divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(4)).toBe(false);
+    });
   });
 
 });
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -1,3 +1,19 @@
-describe('',function() {
-  
-});
+class Javabuzz {
+
+  _isDivisibleBy(number, divisor) {
+    return (number % divisor === 0);
+    
+  }
+
+  isDivisibleByFifteen(number) {
+    return this._isDivisibleBy(number, 15);
+  }
+
+  isDivisibleByThree(number) {
+    return this._isDivisibleBy(number, 3);
+  }
+
+  isDivisibleByFive(number) {
+    return this._isDivisibleBy(number, 15);
+  }
+}
